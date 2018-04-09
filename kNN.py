4805b4c3207656b9b45ab61fc438c648854d48cf
GuoBayern <zhixuan.guo.9536@gmail.com>
2018-04-09 10:26:11 +0800

    新增了一个库，同时产生了图像

--- kNN.py
+++ kNN.py
@@ -1,5 +1,6 @@
 import numpy as np
 import operator
+import matplotlib.pyplot as plt
 def createDataSet():
     group = np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A','A','B','B']
@@ -28,8 +29,13 @@ def file2matrix(filename):
         line = line.strip() #strip()用于移除字符串头尾指定的字符（默认为空格）
         listFromLine = line.split('\t') #split(str="", num=string.count(str))通过指定分隔符对字符串进行切片，如果参数num 有指定值，则仅分隔 num 个子字符串，\t是水平制表（即TAB）
         returnMat[index,:] = listFromLine[0:3]
-        classlabelVector.append(int(listFromLine[-1]))
+        classlabelVector.append(int(listFromLine[-1]))  #append()方法用于在列表末尾添加新的对象，索引值-1表示列表中最后一列元素
         index += 1
     return returnMat,classlabelVector
 group,labels = createDataSet()
 print(classify0([0.5,0.5], group, labels, 3))
+datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
+fig  = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
+plt.show()
